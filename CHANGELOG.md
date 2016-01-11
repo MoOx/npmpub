993d8ae06833971ddcbe3ad919fdc1df72d43c06
Maxime Thirouin <m@moox.io>
2016-01-11 08:02:20 +0100

    Avoid magical --rebase (2.0.0)
    https://github.com/sindresorhus/np/pull/7
    https://github.com/sindresorhus/np/pull/8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
+# 2.0.0 - 2016-01-11
 
+- Changed: do not rebase by default, but instead show a warning if relevant.
 - Fixed: no more ``*-trash`` warnings (``trash`` replaced by ``trash-cli``).
+- Added: show a warning if you have unstashed changes or remote is unreadable.
 
 # 1.0.0 - 2015-10-04
 
--- npmpublish.sh
+++ npmpublish.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env sh
-git pull --rebase &&
+
+if test -n "$(git status --porcelain)"; then
+  echo 'Unclean working tree. Commit or stash changes first.' >&2;
+  exit 128;
+fi
+
+if ! git fetch --quiet 2>/dev/null; then
+  echo 'There was a problem fetching your branch.' >&2;
+  exit 128;
+fi
+
+if test "0" != "$(git rev-list --count --left-only @'{u}'...HEAD)"; then
+  echo 'Remote history differ. Please pull changes.' >&2;
+  exit 128;
+fi
 
 # get the current real path of this file so we can relative node_modules
 # (in our case, trash-cli)
