6c5bdd62c7e7472cbfa959d644f1f5c7ce1f8a7f
Maxime Thirouin <m@moox.io>
2016-01-11 07:57:32 +0100

    Fix usage of local trash utility
    Close #3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+
+- Fixed: no more ``*-trash`` warnings (``trash`` replaced by ``trash-cli``).
+
 # 1.0.0 - 2015-10-04
 
 ✨ Initial release
--- npmpublish.sh
+++ npmpublish.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
-./node_modules/.bin/trash node_modules &>/dev/null;
 git pull --rebase &&
+
+# get the current real path of this file so we can relative node_modules
+# (in our case, trash-cli)
+realPath=$(node -e "console.log(require('path').dirname(require('fs').realpathSync('$0')))")
+
+# run test from a fresh node_modules
+$realPath/node_modules/.bin/trash node_modules
 npm install &&
 npm test &&
 
